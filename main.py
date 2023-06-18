4e629b5fb0224e4241418482057852d45fb29858
Francisco Bórquez <fborquez@outlook.com>
2023-06-18 15:11:52 -0400

    create more functions

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi.responses import HTMLResponse
-from fastapi import FastAPI, Request, HTTPException
-from rich import print
+from fastapi import FastAPI, HTTPException
 import uvicorn
-import requests
-import time
 import sys
 
 from Tor_install import *
@@ -13,25 +10,27 @@ import atexit
 
 
 _connection = None
+tor_proxy = {
+     'http': 'socks5h://localhost:9050',
+     'https': 'socks5h://localhost:9050'
+}
+app = FastAPI()
 
 
-print("[blue]  [=] Checking if Tor installed[/blue]")
-if not check_tor_installed():
-    print("[red]  [-] Tor not found -- >  Installing Tor[/red]")
+@app.get('/{e}', response_class=HTMLResponse)
+async def get(e):
+    if not e.startswith("http://") or e.startswith("https://"):
+        e = "http://" + e
+
     try:
-        install_tor()
-        print("[green]  [+] Installed Tor[/green]")
+        t = requests.get(e, proxies=tor_proxy)
+        return t.text
     except:
-        print("[red]  [-] Tor couldn't be installed -- > Exiting[/red]")
-        exit()
-
-
-print("[green]  [+] Starting Tor [/green]")
-
+        raise HTTPException(status_code=500, detail="Server error")
 
 
 def start_tor():
-
+    print("[green]  [+] Starting Tor [/green]")
 
     if sys.platform.startswith('linux'):
         subprocess.run(['sudo', 'service', 'tor', 'start'])
@@ -46,13 +45,10 @@ def start_tor():
     print("[green]  [+] Tor service restarted successfully.[/green]")
 
 
-start_tor()
-
 def close():
     global _connection
 
     ngrok.disconnect(_connection.public_url)
-
 
 
 def start_ngrok(auth, t):
@@ -70,50 +66,51 @@ def start_ngrok(auth, t):
     atexit.register(close)
 
 
+def try_to_install_tor():
+    print("[blue]  [=] Checking if Tor installed[/blue]")
+    if not check_tor_installed():
+        print("[red]  [-] Tor not found -- >  Installing Tor[/red]")
+        try:
+            install_tor()
+            print("[green]  [+] Installed Tor[/green]")
+        except:
+            print("[red]  [-] Tor couldn't be installed -- > Exiting[/red]")
+            exit()
 
 
-tor_proxy = {
-     'http': 'socks5h://localhost:9050',
-     'https': 'socks5h://localhost:9050'
-}
-
-app = FastAPI()
-
-
-@app.get('/{e}', response_class=HTMLResponse)
-async def get(e):
-    if not e.startswith("http://") or e.startswith("https://"):
-        e = "http://" + e
-    
-    try:
-        t = requests.get(e, proxies=tor_proxy)
-        return t.text
-    except:
-        raise HTTPException(status_code=500, detail="Server error")
-
-if input("Public or Private (1/2): ") == "1":
-    start_ngrok(input("Ngrok Auth : "), input("1 Http or 2 Https (note using https can get 'your ip expossed') : "))
-else:
-    print(f"\n[bold][dark_orange]Private URL : http://localhost:8088[/dark_orange][/bold]\n\n")
-
-print("""[dark_orange]
-        NOTE : 
-            uses port 8088
-            installs tor if not found
-            runs tor service
-
-        Example :
-            ngrok_url:port/<****.onion> or http://localhost:8088/<darkwebspagejknewlkn11232.onion>
-     
-
-            [/dark_orange]""")
+def set_privacy():
+    if input("Public or Private (1/2): ") == "1":
+        start_ngrok(input("Ngrok Auth : "), input("1 Http or 2 Https (note using https can get 'your ip expossed') : "))
+    else:
+        print(f"\n[bold][dark_orange]Private URL : http://localhost:8088[/dark_orange][/bold]\n\n")
 
 
+def run_server():
+    print("""[dark_orange]
+                NOTE :
+                    uses port 8088
+                    installs tor if not found
+                    runs tor service#
 
-uvicorn.run(
-            app,
-            host= "localhost",
-            port= 8088,
-)
+               Example :
+                    ngrok_url:port/<****.onion> or http://localhost:8088/<darkwebspagejknewlkn11232.onion>
+             #
+
+                    [/dark_orange]""")
+
+    uvicorn.run(
+        app,
+        host="localhost",
+        port=8088,
+    )
 
 
+def main():
+    try_to_install_tor()
+    start_tor()
+    set_privacy()
+    run_server()
+
+
+if __name__ == '__main__':
+    main()
